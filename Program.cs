17946ccd6b368ec0382d11f32d95287e2e779e4f
Joseph Bolduc <josephjittin@gmail.com>
2024-10-30 23:12:57 -0500

    Sause engnie

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace Sauce_Engine;
+
+public static class Program
+{
+	private static void Main()
+	{
+		var nativeWindowSettings = new NativeWindowSettings()
+		{
+			ClientSize = new Vector2i(1900, 1040),
+			Title = "hl3.exe",
+		};
+
+		using var window = new Window(GameWindowSettings.Default, nativeWindowSettings);
+		window.Run();
+	}
+}
